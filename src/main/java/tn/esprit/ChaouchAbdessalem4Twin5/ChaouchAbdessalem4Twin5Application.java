a37ccc7a59006e54fb0ed66dd17032c7f43fb635
Slouumaa <abdessalem.chaouch@esprit.tn>
2024-09-30 17:19:55 +0100

    Initial commit

---
+++ src/main/java/tn/esprit/ChaouchAbdessalem4Twin5/ChaouchAbdessalem4Twin5Application.java
@@ -0,0 +1,12 @@
+package tn.esprit.ChaouchAbdessalem4Twin5;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaouchAbdessalem4Twin5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChaouchAbdessalem4Twin5Application.class, args);
+	}
+
+}
---
+++ src/test/java/tn/esprit/ChaouchAbdessalem4Twin5/ChaouchAbdessalem4Twin5ApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.ChaouchAbdessalem4Twin5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChaouchAbdessalem4Twin5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
